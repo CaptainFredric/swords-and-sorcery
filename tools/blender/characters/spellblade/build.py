722f3187533ea0241cba83398a50f960096a4448
Aden Cisneros (Dan DeBugger) <120320495+CaptainFredric@users.noreply.github.com>
2026-09-20 10:16:45 -0500

    Integrate Spellblade rear and side depth polish

--- tools/blender/characters/spellblade/build.py
+++ tools/blender/characters/spellblade/build.py
@@ -17,6 +17,7 @@ from tools.blender.common.export import export_glb
 from tools.blender.common.render import configure_render, look_at, render_still
 from tools.blender.characters.spellblade.animations import build_actions
 from tools.blender.characters.spellblade.concept_model import build_concept_model
+from tools.blender.characters.spellblade.depth_polish import refine_depth_and_back
 from tools.blender.characters.spellblade.design import BODY_HEIGHT
 from tools.blender.characters.spellblade.first_person import build_first_person_asset
 from tools.blender.characters.spellblade.model import build_materials
@@ -221,7 +222,7 @@ def build_character_assets(args: argparse.Namespace) -> None:
     bpy.data.objects.remove(validation_proxy, do_unlink=True)
 
     materials = build_materials()
-    model = build_concept_model(armature, materials)
+    model = refine_depth_and_back(armature, build_concept_model(armature, materials))
     model_report = validate_production_model(armature, model, contract)
     actions = build_actions(armature, contract)
     third_person_animations = list(actions.keys())
