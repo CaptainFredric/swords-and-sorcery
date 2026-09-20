c871423167847f22b1f652c65bf28e607ea0b286
Aden Cisneros (Dan DeBugger) <120320495+CaptainFredric@users.noreply.github.com>
2026-09-19 21:37:59 -0500

    feat: apply concept refinement before Spellblade validation

--- tools/blender/characters/spellblade/build.py
+++ tools/blender/characters/spellblade/build.py
@@ -16,6 +16,7 @@ if str(ROOT) not in sys.path:
 from tools.blender.common.export import export_glb
 from tools.blender.common.render import configure_render, look_at, render_still
 from tools.blender.characters.spellblade.animations import build_actions
+from tools.blender.characters.spellblade.concept_refinement import refine_concept_silhouette
 from tools.blender.characters.spellblade.design import BODY_HEIGHT
 from tools.blender.characters.spellblade.first_person import build_first_person_asset
 from tools.blender.characters.spellblade.model import build_materials, build_third_person_model
@@ -221,6 +222,7 @@ def build_character_assets(args: argparse.Namespace) -> None:
 
     materials = build_materials()
     model = build_third_person_model(armature, materials)
+    model = refine_concept_silhouette(armature, model)
     model_report = validate_production_model(armature, model, contract)
     actions = build_actions(armature, contract)
     third_person_animations = list(actions.keys())
